e4bd02c38e14db4f4c08056b87b7735ce0179051
Nate Ebel <n8ebel@gmail.com>
2019-06-04 21:40:57 -0700

    Setup basic ktor server for local deploy with netty

---
+++ src/Application.kt
@@ -0,0 +1,26 @@
+package org.icp
+
+import io.ktor.application.*
+import io.ktor.http.ContentType
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main(args: Array<String>) {
+    val server = embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("Root Conference Endpoint", ContentType.Text.Html)
+            }
+
+            get("/agenda") {
+                call.respondText("Conference agenda data...", ContentType.Text.Plain)
+            }
+        }
+    }
+    server.start(wait = true)
+}
+
